7066251f03048c6d05ef4675a129aa272a16ebf5
20harriganc <81721678+20harriganc@users.noreply.github.com>
2022-07-15 23:04:00 -0400

    Create simple code.py

---
+++ simple code.py
@@ -0,0 +1,11 @@
+Def Begin():
+  print("Hello World)
+  answer = input("How is your day going? Enter Good or Bad!: ")
+  if answer == "Good":
+      print("I hope it continues!")
+  else:
+      print("Don't give up yet anyday can turn into a good day!")
+      
+ 
+Begin()
+  
